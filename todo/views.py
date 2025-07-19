44a01ed84e08bc9685459be2b477faa228a8a4cb
KhairiddinSakhiev <sakhievkhairiddin2000@gmail.com>
2025-07-19 20:14:31 +0500

    all done

--- todo/views.py
+++ todo/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render, HttpResponse, redirect
-
+from datetime import datetime
 from .models import *
 
 def task_list_view(request):
     if request.method == "GET":
-        tasks = Task.objects.all().order_by("-id")
+        tasks = Task.objects.filter(user=request.user).order_by("-id")
+        search = request.GET.get("search", False)
+        if search:
+            tasks = tasks.filter(title=search)
+        today = datetime.strftime(datetime.now(), "%Y-%m-%d")
+        due_date = request.GET.get("due_date", today)
+        if due_date:
+            tasks = tasks.filter(due_date__date=due_date)
+        is_completed = request.GET.get("is_completed", False)
+        if is_completed == "on":
+            tasks = tasks.filter(is_completed=True)
         return render(request, "task_list.html", {"tasks":tasks})
 
 
@@ -16,17 +26,15 @@ def task_create_view(request):
         title = request.POST.get("title", False)
         description = request.POST.get("description", False)
         due_date = request.POST.get("due_date", False)
-        user_id = request.POST.get("user", False)
-        if not title or not due_date or not user_id:
+        if not title or not due_date:
             return render(request, "task_create.html",
                            {"title":title, 
                             "description":description,
                             "due_date":due_date})
-        user = User.objects.filter(id=user_id).first()
         Task.objects.create(
             title=title,
             description=description,
-            user = user,
+            user = request.user,
             due_date=due_date
         )
         return redirect("task_list")
@@ -67,3 +75,13 @@ def task_delete_view(request, pk):
     elif request.method == "POST":
         task.delete()
         return redirect("task_list")
+
+
+def complete_task_view(request, pk):
+    task = Task.objects.filter(id=pk).first()
+    if not task:
+        return HttpResponse("task not found")
+    task.is_completed = True
+    task.save()
+    return redirect("task_list")
+
